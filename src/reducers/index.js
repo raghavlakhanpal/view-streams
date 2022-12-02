752b4a65190daefd0a17b215bbc85f453ea64de5
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2022-12-02 22:10:09 +0530

    added validation and form input

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
+//importing reducer from redux-form
+//to add form data to store object
+import { reducer as formReducer } from "redux-form";
 
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
